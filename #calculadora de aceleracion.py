99b7910f9e4cf1341f2fc622e94a125b68e59920
2024-0313-ux <2024-0313@unad.edu.do>
2025-11-10 19:46:04 -0400

    Delete #calculadora de aceleracion.py

--- #calculadora de aceleracion.py
+++
@@ -1,8 +0,0 @@
-#calculadora de aceleracion
-print("ejemplo: m/s o km/h")
-medida=input("medida: " )
-vf=float(input("velocidad final: "))
-vi=float(input("velocidad inicial:"))
-t=float(input("tiempo"))
-aceleracion=(vf-vi)/t
-print("resultado: ", aceleracion, " ""m/s2")
